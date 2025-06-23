fc438c71496b35f38782d53e517d5004595e4ed7
LeeHwaRang <84739055+hwarang98@users.noreply.github.com>
2025-06-23 10:28:57 +0900

    ADD: 트롤 몬스터 추가

---
+++ ConsoleRPG/Enemy/Monster/Troll/Troll.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "../Interface/MonsterInterface.h"
+#include <string>
+
+class Troll : public IMonster
+{
+public:
+  Troll(int level);
+
+  std::string GetName() const override;
+  int GetHealth() const override;
+  int GetAttack() const override;
+  void TakeDamage(int damage) override;
+
+private:
+  std::string name;
+  int health;
+  int attack;
+};
