8ceb3752f922ac36f924cc54235a75231f7db02f
Seonho Kim <dev.sensational@gmail.com>
2025-06-23 15:49:05 +0900

    Add: EventType Enum

--- ConsoleRPG/ConsoleRPG.h
+++ ConsoleRPG/ConsoleRPG.h
@@ -1,10 +1,18 @@
-#pragma once
+﻿#pragma once
 
 #define TEXT(X) make_shared<string>(X)
 
-enum EventType
+enum EEventType
 {
-	CharacterTakeDamage = 0,
+	/*
+	*	캐릭터가 데미지를 받을 때 호출(예시: 몬스터가 Attack()으로 Broadcast, 캐릭터가 TakeDamage()를 Subscribe)
+	*/
+	EET_CharacterTakeDamage = 0, 
+	/*
+	*	몬스터가 데미지를 받을 때 호출
+	*/
+	EET_MonsterTakeDamage, 
+	
 
-	end = 99,
+	EET_END = 99,
 };
