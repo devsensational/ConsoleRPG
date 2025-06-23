6e7808c41b7aee2d80c7d60737aeffb979532471
Seonho Kim <dev.sensational@gmail.com>
2025-06-23 17:28:32 +0900

    Refactor: ConsoleRPG Enum

--- ConsoleRPG/ConsoleRPG.h
+++ ConsoleRPG/ConsoleRPG.h
@@ -1,4 +1,4 @@
-#pragma once
+#pragma once
 
 #define TEXT(X) make_shared<string>(X)
 
@@ -7,7 +7,7 @@ enum EEventType
 	/*
 	*	캐릭터가 데미지를 받을 때 호출(예시: 몬스터가 Attack()으로 Broadcast, 캐릭터가 TakeDamage()를 Subscribe)
 	*/
-	EET_CharacterTakeDamage = 0, 
+	EET_CharacterTakeDamage,
 	/*
 	*	몬스터가 데미지를 받을 때 호출
 	*/
