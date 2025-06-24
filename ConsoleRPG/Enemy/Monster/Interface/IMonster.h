abd99c0dc92305b224f76c0abdd4ebe52b1666b8
J_J <bear3007@naver.com>
2025-06-24 21:15:10 +0900

    IMonster 수정

--- ConsoleRPG/Enemy/Monster/Interface/IMonster.h
+++ ConsoleRPG/Enemy/Monster/Interface/IMonster.h
@@ -8,5 +8,4 @@ public:
   virtual std::string GetName() const = 0;
   virtual int GetCurrentMonsterHealth() const = 0;
   virtual int GetMaxMonsterHealth() const = 0;
-  virtual int GetAttack() const = 0;
 };
