1dc0ca2f7bbca1a13c6ac3a6224a5dafa80cd739
LeeHwaRang <84739055+hwarang98@users.noreply.github.com>
2025-06-25 14:57:45 +0900

    주석삭제

--- ConsoleRPG/Enemy/Monster/Boss/CRDragon.h
+++ ConsoleRPG/Enemy/Monster/Boss/CRDragon.h
@@ -6,11 +6,3 @@ class Dragon : public MonsterBase
 public:
     Dragon(int level, int uniqueId);
 };
-
-
-// class Orc : public MonsterBase {
-// public:
-//     Orc(int level, int uniqueId);
-// };
-
-
