094f3aea35809eb233200e51b51baa74cbbd5166
LeeHwaRang <84739055+hwarang98@users.noreply.github.com>
2025-06-25 10:19:41 +0900

    fix: 몬스터 생성 시 `unique_ptr` 에서 대신 `shared_ptr` 을 사용

--- ConsoleRPG/Enemy/Monster/Factory/CRMonsterFactory.cpp
+++ ConsoleRPG/Enemy/Monster/Factory/CRMonsterFactory.cpp
@@ -3,23 +3,32 @@
 #include "../Goblin/CRGoblin.h"
 #include "../Orc/CROrc.h"
 #include "../Slime/CRSlime.h"
-#include "../Enum/CRMonsterType.h"
 
-std::unique_ptr<MonsterBase> CRMonsterFactory::CreateMonster(CRMonsterFactory monsterType, int level)
+
+/**
+ * @brief 지정된 몬스터 유형과 레벨에 따라 몬스터 객체를 생성합니다.
+ *
+ * @param monsterType 생성할 몬스터의 유형입니다. EMonsterType 열거형을 사용합니다.
+ *        가능한 값: EMT_Troll, EMT_Goblin, EMT_Orc, EMT_Slime.
+ * @param level 생성될 몬스터의 레벨을 나타냅니다. 정수 값이어야 합니다.
+ * @return std::shared_ptr<MonsterBase> 생성된 몬스터 객체를 가리키는 스마트 포인터.
+ *         유효하지 않은 유형이 사용될 경우 nullptr을 반환합니다.
+ */
+std::shared_ptr<MonsterBase> CRMonsterFactory::CreateMonster(EMonsterType monsterType, int level)
 {
   switch (monsterType)
   {
   case EMonsterType::EMT_Troll:
-    return std::make_unique<Troll>(level);
+    return std::make_shared<Troll>(level);
     
   case EMonsterType::EMT_Goblin:
-    return std::make_unique<Goblin>(level);
+    return std::make_shared<Goblin>(level);
 
   case EMonsterType::EMT_Orc:
-    return std::make_unique<Orc>(level);
+    return std::make_shared<Orc>(level);
 
   case EMonsterType::EMT_Slime:
-    return std::make_unique<Slime>(level);
+    return std::make_shared<Slime>(level);
   
   default:
     return nullptr;
--- ConsoleRPG/Enemy/Monster/Factory/CRMonsterFactory.h
+++ ConsoleRPG/Enemy/Monster/Factory/CRMonsterFactory.h
@@ -1,25 +1,21 @@
 ﻿#pragma once
 #include "../Interface/CRMonsterBase.h"
+#include "../Enum/CRMonsterType.h"
 #include <memory>
 
 class CRMonsterFactory
 
 {
-public:
     /**
-     * @brief 몬스터 타입에 따라 해당하는 몬스터 인스턴스를 생성
-     * 
-     * @param monsterType 생성할 몬스터의 타입
-     * @param level 몬스터의 레벨 (몬스터의 기본 능력치에 영향을 줌)
-     * @return std::unique_ptr<MonsterBase> 생성된 몬스터 인스턴스.
-     *         알 수 없는 몬스터 타입인 경우 nullptr 반환
-     * 
-     * @note 반환된 포인터는 unique_ptr로 관리되어 자동으로 메모리가 해제됩니다
-     * @see EMonsterType
-     * @see MonsterBase
+     * @brief 지정된 몬스터 유형과 레벨에 따라 몬스터 객체를 생성합니다.
+     *
+     * @param monsterType 생성할 몬스터의 타입 (EMonsterType 열거형 값)
+     * @param level 생성될 몬스터의 레벨
+     * @return std::shared_ptr<MonsterBase> 생성된 몬스터 객체를 가리키는 스마트 포인터입니다.
+     *         유효하지 않은 타입이 전달될 경우 nullptr을 반환합니다.
      */
-
-    static std::unique_ptr<MonsterBase> CreateMonster(CRMonsterFactory monsterType, int level);
+public:
+    static std::shared_ptr<MonsterBase> CreateMonster(EMonsterType monsterType, int level);
 
 private:
   /** @brief 팩토리 클래스의 인스턴스화를 방지하기 위한 private 생성자 */
